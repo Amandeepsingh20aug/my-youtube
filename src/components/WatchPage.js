ac343d60adc4d23cd75193babce176023af2b297
amandeepsingh1929 <as0632568@gmail.com>
2024-06-23 14:28:33 +0530

    Applied the nested cmnts logic and buil the UI for it .

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useParams, useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -11,6 +12,7 @@ const WatchPage = () => {
     dispatch(closeMenu(false));
   }, []);
   return (
+    <div className="flex flex-col">
     <div className="px-5 py-3">
       <iframe
         width="1200"
@@ -22,6 +24,8 @@ const WatchPage = () => {
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
+    </div>
+    <CommentsContainer/>
     </div>
   );
 };
